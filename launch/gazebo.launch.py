c985063b45eaecd04190e666c5dd1d99556255d2
Aakash Dammala <dammala.aakash@gmail.com>
2025-11-29 19:27:47 -0500

    Added gazebo world with realsense sensor 1. Choose newer ignition gazebo 2. 4m x 2m x 0.5m box acts as a table 3. Added realsense sensor with depth and pointcloud output 4. Gazbo ros bridge to take output from gazebo and visualize in rviz

---
+++ launch/gazebo.launch.py
@@ -0,0 +1,45 @@
+import os
+from ament_index_python.packages import get_package_share_directory # Import this
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('paint_cloud')
+    sdf_file = os.path.join(pkg_share, 'worlds', 'realsense_world.sdf')
+
+    ign_gazebo = ExecuteProcess(
+        cmd=['ign', 'gazebo', '-r', sdf_file],
+        output='screen'
+    )
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/realsense/image@sensor_msgs/msg/Image[ignition.msgs.Image',
+            '/realsense/depth_image@sensor_msgs/msg/Image[ignition.msgs.Image',
+            '/realsense/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
+            '/realsense/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+        ],
+        output='screen'
+    )
+
+    static_tf = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '1.5', '0', '1.5708', '0', 'world', 'realsense_camera/link/realsense_d435']
+    )
+
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        ign_gazebo,
+        bridge,
+        static_tf,
+        rviz
+    ])
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'paint_cloud'
@@ -9,7 +11,14 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+
         ('share/' + package_name, ['package.xml']),
+
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+
+        # Install world files
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
